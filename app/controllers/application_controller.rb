61c14888d0d66a7c4466b6dd0ff5202e16bc2749
Daniel Urzua Bozza <67697356+DanielUrzuaBozza@users.noreply.github.com>
2023-08-31 00:11:18 -0300

    Update app/controllers/application_controller.rb
    Co-authored-by: Lucas Bonnefon <ryxtor@outlook.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    # Permit the first_name and last_name parameters along with other
-    # sign up parameters.
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :birth_date, :explorer, :admin])
 
     # Permit the first_name and last_name parameters along with other
