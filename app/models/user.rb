793bc4470ec5cde21e001194520e113f4c4a4ab6
Daniel Urzua Bozza <67697356+DanielUrzuaBozza@users.noreply.github.com>
2023-09-01 21:48:16 -0300

    Update app/models/user.rb
    Co-authored-by: Lucas Bonnefon <ryxtor@outlook.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :gender, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\Z/i }, uniqueness: true
   enum gender: { Male: 0, Female: 1, Non_Binary: 2, }, _prefix: true
-  validates :username, length: { in: 3..20 }
+  validates :username, length: { in: 3..16 }, uniqueness: true
 end
