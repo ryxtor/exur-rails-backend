5e3f93ad35335e0b1b03c28a4032514b458e6676
Daniel-U-B <daniel.urzua.bozza@gmail.com>
2023-09-12 00:11:35 -0300

    Configure cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  load_and_authorize_resource unless: :devise_controller?
+
   respond_to :json
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -10,5 +12,8 @@ class ApplicationController < ActionController::API
 
     devise_parameter_sanitizer.permit(:account_update,
                                       keys: %i[first_name last_name gender birth_date explorer admin username])
+  end
+  rescue_from CanCan::AccessDenied do
+    render json: { error: 'You are not authorized to access this page.' }, status: :unauthorized
   end
 end
